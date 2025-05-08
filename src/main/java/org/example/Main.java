01357586dfd0646e4e702795fd6da8de7b0a0593
Cem Furkan Kartoğlu <cemfurkankartoglu@gmail.com>
2025-05-08 18:16:26 +0300

    all tests passed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,33 +6,35 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        if (!isBarking) {
+        if (!isBarking || clock < 0) {
             return false;
-        } else if (clock < 8 || clock > 20) {
-            return true;
-        } else {
-            return false;
-        }
+        } else return clock < 8 || clock > 20;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       if ((firstAge > 13 && firstAge < 19) || (secondAge > 13 && secondAge < 19) || (thirdAge > 13 && thirdAge < 19) ) {
-           return true;
-       } else {
-               return false;
-           }
+        return (firstAge > 13 && firstAge <= 19) || (secondAge > 13 && secondAge <= 19) || (thirdAge > 13 && thirdAge <= 19);
        }
 
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if (isSummer && temp > 25 && temp < 45) {
+            return true;
+        } else return !isSummer && temp > 25 && temp <= 35;
     }
 
     public static double area(double width, double height) {
-       
+       if (width < 0 || height <0) {
+           return -1;
+       } else {
+           return width * height;
+       }
     }
 
     public static double area(double radius) {
-       
+       if (radius < 0) {
+           return -1;
+       } else {
+           return radius * radius * Math.PI;
+       }
     }
 }
